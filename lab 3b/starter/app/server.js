ce995d76cb23adf0b549c5b270fc0ca2c6468b3e
ms5788 <ms5788@drexel.edu>
2026-07-09 22:41:58 -0400

    Refactor random number generation endpoint to include validation for min and max parameters on server side

--- lab 3b/starter/app/server.js
+++ lab 3b/starter/app/server.js
@@ -12,13 +12,35 @@ function getRandomIntegerInRange(min, max) {
   return Math.floor(Math.random() * (max + 1 - min) + min);
 }
 
+
+function validateMinMax(minStr, maxStr) {
+    const min = parseInt(minStr);
+    const max = parseInt(maxStr);
+    if (Number.isNaN(min) || Number.isNaN(max)) {
+      throw new Error("min and max must be valid numbers");
+    }
+
+    if (min > max) {
+      throw new Error("min cannot be bigger than max");
+    }
+
+    return { min, max };
+}
+
 app.get("/random", (req, res) => {
-  console.log("Received query string:", req.query);
-  const min = parseInt(req.query.min);
-  const max = parseInt(req.query.max);
-  let randomNumber = getRandomIntegerInRange(min, max);
-  console.log("Selected random number", randomNumber);
-  res.json({ number: randomNumber });
+  try{   
+    console.log("Received query string:", req.query);
+    const { min, max } = validateMinMax(req.query.min, req.query.max);
+
+    let randomNumber = getRandomIntegerInRange(min, max);
+    console.log("Selected random number", randomNumber);
+    res.json({ number: randomNumber });
+
+  } catch (error) {
+    console.error("Validation error:", error.message);
+    res.status(400).json({ error: error.message});
+  }
+
 });
 
 app.listen(port, hostname, () => {
