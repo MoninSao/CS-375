9a2fa56296b5bf63ba4468c1d854644985feb1ba
ms5788 <ms5788@drexel.edu>
2026-07-14 14:47:28 -0400

    Enhance UI and functionality of weather app: add styling, improve table structure, and update server response handling

--- Lab 4a/starter/app/server.js
+++ Lab 4a/starter/app/server.js
@@ -12,13 +12,22 @@ let hostname = "localhost";
 let baseUrl = "https://api.openweathermap.org/data/2.5/weather";
 
 app.get("/feels-like", (req, res) => {
-  let zip = 19104;
-  let url = `${baseUrl}?zip=${zip}&appid=${apiKey}`;
-  axios.get(url).then((response) => {
-    console.log("Received response:", response.data);
-    res.json(response.data);
-  });
-  console.log(`Sending request to: ${url}`);
+
+  try {
+    const zip = req.query.zip;
+    let url = `${baseUrl}?zip=${zip}&appid=${apiKey}`;
+    axios.get(url).then((response) => {
+      console.log("Received response:", response.data);
+      let ZIP = zip;
+      let TMP = (response.data.main.feels_like - 273.15) * 9/5 + 32;
+      res.json( {"zip": ZIP, "feels-like-fahrenheit": TMP } );
+    });
+    console.log(`Sending request to: ${url}`);
+  } catch (error) {
+    console.error("Validation error:", error.message);
+    res.status(400).json({error: error.message });
+  }
+
 });
 
 app.listen(port, hostname, () => {
