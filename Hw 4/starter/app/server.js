3fa2ca16499849f48f0f29b4d240de12ee435d5c
ms5788 <ms5788@drexel.edu>
2026-07-22 20:08:26 -0400

    Implement weather forecast feature with error handling in server.js and update index.html for user input and display

--- Hw 4/starter/app/server.js
+++ Hw 4/starter/app/server.js
@@ -9,6 +9,24 @@ let hostname = "localhost";
 app.use(express.static("public"));
 // don't change code above this line
 
+app.get("/forecast", (req, res) => {
+  let zip = req.query.zip;
+  axios
+    .get(`${baseUrl}?appid=${apiKey}&zip=${zip}`)
+    .then((response) => {
+      res.status(200).json(response.data);
+    })
+    .catch((error) => {
+      if (error.response) {
+        res
+          .status(error.response.status)
+          .json({ error: error.response.data.message });
+      } else {
+        res.status(500).json({ error: "Internal server error" });
+      }
+    });
+});
+
 app.listen(port, hostname, () => {
   console.log(`http://${hostname}:${port}`);
 });
