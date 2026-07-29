ed6f59a49b5218254f584658f603e85d808dab07
ms5788 <ms5788@drexel.edu>
2026-07-29 19:03:33 -0400

    Refactor server.js to streamline book addition and search functionality

--- Hw 5/starter/app/server.js
+++ Hw 5/starter/app/server.js
@@ -1,21 +1,37 @@
-const pg = require("pg");
-const express = require("express");
-const app = express();
+app.use(express.json());
 
-const port = 3000;
-const hostname = "localhost";
+const GENRES = ["scifi", "romance", "adventure"];
 
-const env = require("../env.json");
-const Pool = pg.Pool;
-const pool = new Pool(env);
-pool.connect().then(function () {
-  console.log(`Connected to database ${env.database}`);
+app.post("/add", (req, res) => {
+  let { title, genre, quality } = req.body;
+
+  if (
+    typeof title !== "string" || title.length < 1 || title.length > 15 ||
+    !GENRES.includes(genre) ||
+    (quality !== "yes" && quality !== "no")
+  ) {
+    return res.status(400).end();
+  }
+
+  pool
+    .query("INSERT INTO books (title, genre, quality) VALUES ($1, $2, $3)",
+           [title, genre, quality])
+    .then(() => res.status(200).end())
+    .catch((err) => { console.log(err); res.status(400).end(); });
 });
 
-app.use(express.static("public"));
+app.get("/search", (req, res) => {
+  let genre = req.query.genre;
 
-/* YOUR CODE HERE */
-
-app.listen(port, hostname, () => {
-  console.log(`Listening at: http://${hostname}:${port}`);
+  if (!GENRES.includes(genre)) {
+    pool
+      .query("SELECT * FROM books")
+      .then((result) => res.status(200).json({ rows: result.rows }))
+      .catch((err) => { console.log(err); res.status(200).json({ rows: [] }); });
+  } else {
+    pool
+      .query("SELECT * FROM books WHERE genre = $1", [genre])
+      .then((result) => res.status(200).json({ rows: result.rows }))
+      .catch((err) => { console.log(err); res.status(200).json({ rows: [] }); });
+  }
 });
